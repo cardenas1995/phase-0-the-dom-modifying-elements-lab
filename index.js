0143b500f04bdadfcceab5fd5f339f8ad47c3223
Kevin <k.cardenas1795@gmail.com>
2024-01-20 16:28:24 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "YOUR-NAME is the champion";
